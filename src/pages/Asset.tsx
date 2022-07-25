f1b5ea55ae01c003ae51df237a00ffd0a07ba603
Alec Parkes <57089930+AlanBark@users.noreply.github.com>
2022-07-26 02:14:59 +1000

    updated packages

---
+++ src/pages/Asset.tsx
@@ -0,0 +1,13 @@
+import React, {useState} from 'react'
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/react'
+
+const Asset = () => {
+
+  return (
+    <IonPage>
+      <p>{}</p>
+    </IonPage>
+  )
+}
+
+export default Asset
--- src/pages/mobile/QrScan.tsx
+++ src/pages/mobile/QrScan.tsx
@@ -1,24 +1,30 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/react';
-import React, { useState } from 'react';
-import { BarcodeScanner, BarcodeScannerOptions } from '@awesome-cordova-plugins/barcode-scanner';
+import React, { useState, useEffect } from 'react';
+import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
+import { useHistory } from 'react-router-dom';
 
+/*
+  * BarcodeScanner is a plugin for Capacitor used here to scan QR Codes
+  * Note that this page is mobile only, browser should never redirect here as it is not supported 
+*/
 const QrScan = () => {
 
-  const [data, setData] = useState('No QR code.');
+  const [data, setData] = useState<string>();
 
-  const openScanner = async () => {
-    const scannerOptions: BarcodeScannerOptions = {
-      formats: 'QR_CODE',
-      preferFrontCamera: true,
-      prompt: "Scan Asset QR code"
+  const startScan = async () => {
+    BarcodeScanner.hideBackground(); 
+  
+    const result = await BarcodeScanner.startScan();
+  
+    if (result.hasContent) {
+      
+      setData(result.content);
     }
-    const data = await BarcodeScanner.scan(scannerOptions);
-    console.log(`Barcode data: ${data.text}`);
-  }
+  };
 
   return (
     <IonPage>
-      <IonButton onClick={openScanner}>Scan</IonButton>
+      <IonButton onClick={startScan}>Scan Asset</IonButton>
     </IonPage>
   );
 };
