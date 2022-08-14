f249ad77a2527dd467053854d738b37a0df6af95
Isaac Woodrow <106070777+isaacWoodrowQUT@users.noreply.github.com>
2022-08-14 12:45:24 +1000

    Basic Implementation of List
    Not working though

--- src/pages/AssetPage.tsx
+++ src/pages/AssetPage.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { IonButton, IonPage, IonContent  } from '@ionic/react'
+import BackButton from '../components/BackButton'
 
 const AssetPage = () => {
   return (
     <IonPage>
       <IonContent>
-      <IonButton color="medium" href="/home">BACK</IonButton>
+      <BackButton text="back" />
       <h1>Asset Page</h1>
       <p>This would be the page of a specfic item, so it would use the get assetquery, 
         or carry the asset info from previous page</p>
--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,18 +1,68 @@
-import React from 'react'
-import { IonButton, IonPage, IonBackButton, IonContent  } from '@ionic/react'
+import React, { useEffect, useState } from 'react'
+import { IonButton, IonPage, IonBackButton, IonContent, IonHeader, IonToolbar, IonTitle, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonItem, IonIcon, IonLabel, IonThumbnail  } from '@ionic/react'
+import BackButton from '../components/BackButton'
+import { pin, wifi, wine, warning, walk, } from 'ionicons/icons';
+import { listAssets } from '../graphql/queries';
+import { API } from 'aws-amplify';
+
+
 
 const Search = () => {
+  const [assets, setAssets] = useState([]);
+  const listAsset = async (): Promise<void> => {
+    try {
+      const result: any = await API.graphql({
+        query: listAssets,
+        authMode: 'AWS_IAM'
+      });
+      console.log(result);
+      setAssets(result.data.listAssets.items);
+    } catch (e) {
+      console.log(e);
+    }
+    return;
+  };
+
+
   return (
     <IonPage>
-      <h1>Search</h1>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>SEARCH</IonTitle>
+        </IonToolbar>
+      </IonHeader>
       <IonContent>
-      <IonButton color="medium" href="/home">BACK</IonButton>
-      
-
+      <BackButton text="back" />
+        <IonCard>
+          <IonItem detail routerLink="/AssetPage">
+            
+          <IonThumbnail slot="start">
+            <img src="https://www.australiancomputertraders.com.au/assets/full/HP850G5i52-r.jpg?20220226055643" />
+          </IonThumbnail>
+          <IonCardHeader>
+          <IonCardSubtitle color="danger">On Loan</IonCardSubtitle>
+            <IonCardTitle>HP Laptop</IonCardTitle>
+            <IonIcon name="arrow-dropright" size="large" color="white" slot="end"></IonIcon>
+            </IonCardHeader>
+            
+          </IonItem>
+        </IonCard>
+        <IonButton color="primary" onClick={(event: React.MouseEvent<HTMLElement>) => {listAsset()}} >List Assets</IonButton>
+        {
+          assets.map((asset: any) => {
+            return (
+              <div key={asset.id}>
+                <p style={{display: "inline", marginLeft: "20px"}}>{asset.id}</p>
+                <p style={{display: "inline", marginLeft: "20px"}}>{asset.name}</p>
+                <p style={{display: "inline", marginLeft: "20px"}}>{asset.description}</p>
+              </div>
+            )
+          })
+        }
       </IonContent>
-      
     </IonPage>
   )
 }
+
 
 export default Search
