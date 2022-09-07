49e6e35dd2e351b4bdeeb37c15576f1e49051424
Alec Parkes <57089930+AlanBark@users.noreply.github.com>
2022-09-07 15:51:16 +1000

    commit before react update

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from 'react'
-import { IonButton, IonPage, IonBackButton, IonContent, IonHeader, IonToolbar, IonTitle, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonItem, IonIcon, IonLabel, IonThumbnail, IonSearchbar } from '@ionic/react'
+import { IonButton, IonPage, IonBackButton, IonContent, IonHeader, IonToolbar, IonTitle, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonItem, IonIcon, IonLabel, IonThumbnail, IonSearchbar, useIonRouter } from '@ionic/react'
 import BackButton from '../components/BackButton'
 import { pin, wifi, wine, warning, walk, } from 'ionicons/icons';
 import { listAssetLocations, listAssets, listAssetStatuses, listAssetTypes } from '../graphql/queries';
 import { API } from 'aws-amplify';
 import { Link } from 'react-router-dom';
+import { AgGridReact } from 'ag-grid-react'
 import SearchItem from '../components/SearchItem';
 import Header from '../components/Header';
 import Selector from '../components/Selector';
+
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-alpine.css";
 
 interface searchProps {
   user: any;
@@ -26,13 +30,34 @@ const Search = ({ user }: searchProps) => {
   const [searchStatus, setSearchStatus] = useState({ name: null, id: null });
   const [searchLocation, setSearchLocation] = useState({ name: null, id: null });
 
+  const [columnDefs, setColumnDefs] = useState([
+    { headerName: "Name", field: "name", sortable: true, filter: true, flex: 1 },
+    { headerName: "Type", field: "type", sortable: true, filter: true, flex: 1 },
+    { headerName: "Status", field: "status", sortable: true, filter: true, flex: 1 },
+    { headerName: "Location", field: "location", sortable: true, filter: true, flex: 1 },
+    { headerName: "Updated", field: "updated", sortable: true, filter: true, flex: 1 }
+  ]);
+
+  const router = useIonRouter();
+
   const listAsset = async (): Promise<void> => {
     try {
       const result: any = await API.graphql({
         query: listAssets,
         authMode: 'AWS_IAM'
       });
-      setAssets(result.data.listAssets.items);
+      let formatted = result.data.listAssets.items.map((asset: any) => {
+        return {
+          id: asset.id,
+          name: asset.assetName,
+          type: asset.typeID,
+          status: asset.statusID,
+          location: asset.assetlocaID,
+          updated: asset.updatedAt
+        }
+      });
+      setAssets(formatted);
+      console.log(formatted);
     } catch (e) {
       console.log(e);
     }
@@ -41,35 +66,38 @@ const Search = ({ user }: searchProps) => {
 
   useEffect(() => {
     listAsset();
-  } , []);
+  }, []);
 
   useEffect(() => {
-    const filtered = assets.filter((asset: any) => {
-      if (search !== '' && !asset.assetName.toLowerCase().includes(search.toLowerCase())) {
-        return false;
-      } else {
-        if (searchType.id !== null && asset.typeID !== searchType.id) {
+    if (assets && assets.length > 0) {
+      let filtered = assets.filter((asset: any) => {
+        if (search !== '' && !asset.name.toLowerCase().includes(search.toLowerCase())) {
           return false;
         } else {
-          if (searchStatus.id !== null && asset.statusID !== searchStatus.id) {
+          if (searchType.id !== null && asset.type !== searchType.id) {
             return false;
           } else {
-            if (searchLocation.id !== null && asset.assetlocaID !== searchLocation.id) {
+            if (searchStatus.id !== null && asset.status !== searchStatus.id) {
               return false;
             } else {
-              return true;
+              if (searchLocation.id !== null && asset.location !== searchLocation.id) {
+                return false;
+              } else {
+                return true;
+              }
             }
           }
         }
-      }
-    });
-    console.log(filtered);
-    setFilteredAssets(filtered);
-  }, [search, searchStatus, searchType, searchLocation]);
+      });
+      console.log(filtered)
+    }
+  }, [search, searchStatus, searchType, searchLocation, assets]);
+
+  const rowStyle = { cursor: 'pointer' };
 
   return (
     <IonPage>
-      <Header title={"Search Assets"} user={user}/>
+      <Header title={"Search Assets"} user={user} />
       <IonContent>
         <div className="bg-white p-2 m-4 rounded-lg shadow">
           <div className='flex'>
@@ -77,16 +105,18 @@ const Search = ({ user }: searchProps) => {
             <Selector label="Status" queryType={listAssetStatuses} handleChange={setSearchStatus} nameKey="statusName" />
             <Selector label="Type" queryType={listAssetTypes} handleChange={setSearchType} nameKey="typeName" />
             <Selector label="Location" queryType={listAssetLocations} handleChange={setSearchLocation} nameKey="locationName" />
-            
+
           </div>
-        {
-          filteredAssets.map((asset: any) => {
-            return (
-              <SearchItem key={asset.id} asset={asset} />
-              )
-            })
-          }
-          </div>
+          <AgGridReact
+            domLayout={'autoHeight'}
+            columnDefs={columnDefs}
+            rowData={filteredAssets}
+            pagination={true}
+            paginationPageSize={10}
+            onRowClicked={(row) => {console.log(row)}}
+            rowStyle={rowStyle}
+            />
+        </div>
       </IonContent>
     </IonPage>
   )
