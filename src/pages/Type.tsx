d44e5376e72276d1f03f1d70a7fa42604bc3dec6
Stormers64 <joshdaniell@icloud.com>
2022-10-01 16:48:40 +1000

    Clean up type page code

--- src/pages/Type.tsx
+++ src/pages/Type.tsx
@@ -40,23 +40,12 @@ interface FieldsInterface {
 const Type: React.FC<AssetProps> = ({ match }) => {
     const [presentAlert] = useIonAlert();
     // user input
-    const [name, setName] = useState('');
-    const [description, setDescription] = useState('');
     const [type, setType] = useState({ typeName: '', id: undefined, dataTemplate: '', logTemplate: '' });
-    const [status, setStatus] = useState({ name: '', id: undefined });
-    const [location, setLocation] = useState({ name: '', id: undefined });
-    const [group, setGroup] = useState(null);
     const [assetTypeData, setAssetTypeData] = useState(Array<FieldsInterface>());
 
     // dynamic field states
     const [typeFields, setTypeFields] = React.useState(Array<FieldInputs>());
     const [logFields, setLogFields] = React.useState(Array<FieldInputs>());
-
-    // array of all possible statuses
-    const [allStatuses, setAllStatuses] = useState(Array<Status>());
-
-    // array of all possible types
-    const [allTypes, setAllTypes] = useState(Array<Type>());
 
     // utility vars
     const [loaded, setLoaded] = useState(false);
@@ -78,30 +67,11 @@ const Type: React.FC<AssetProps> = ({ match }) => {
         console.log(type.typeName + "Hello world");
 
         let typeDetails = {
-            id: "9f72c051-203b-4712-a2c3-fccd64ec8550",
+            id: match.params.id,
             typeName: type.typeName, //change this to type name (i think its using asset name)
             dataTemplate: assetFieldsJSON,
             logTemplate: assetLogFieldsJSON
         }
-
-        // const createType = async (): Promise<void> => {
-        //     try {
-        //         const result: any = await API.graphql({
-        //             query: createAssetType,
-        //             variables: { input: typeDetails },
-        //             authMode: 'AWS_IAM'
-        //         });
-        //         // @TODO Success or error toast here
-        //         console.log(result);
-        //         console.log("test work");
-        //         router.goBack();
-        //     } catch (e) {
-        //         console.log(e);
-        //     }
-        //     return;
-        // };
-
-        // createType();
 
         const updateType = async (): Promise<void> => {
             try {
@@ -121,12 +91,6 @@ const Type: React.FC<AssetProps> = ({ match }) => {
         };
 
         updateType();
-    }
-
-    const removeField = (index: number) => {
-        let newFields = [...typeFields];
-        newFields.splice(index, 1);
-        setTypeFields(newFields);
     }
 
     useEffect(() => {
@@ -175,35 +139,7 @@ const Type: React.FC<AssetProps> = ({ match }) => {
     }, [assetTypeData, type]);
 
     useEffect(() => {
-        // fetch asset and construct form with existing data
-        const fetchAsset = async (): Promise<void> => {
-            try {
-                const result: any = await API.graphql({
-                    query: getAsset,
-                    variables: { id: match.params.id },
-                    authMode: 'AWS_IAM'
-                });
-                let asset = result.data.getAsset;
-                console.log(asset)
-                if (!asset) {
-                    throw new Error('Asset ID not found');
-                }
-                // wait for all data, then populate type form
-                Promise.all([
-                    setName(asset.assetName),
-                    setDescription(asset.description),
-                    fetchType(asset.typeID),
-                    setAssetTypeData(JSON.parse(asset.assetTypeData))
-                ]).then(() =>
-                    setLoaded(true)
-                );
-            } catch (e: any) {
-                setLoaded(true);
-                setError(e.message);
-            }
-            return;
-        }
-
+        // fetch type and construct form with existing data
         const fetchType = async (typeId: string): Promise<void> => {
             if (typeId) {
                 try {
@@ -222,18 +158,8 @@ const Type: React.FC<AssetProps> = ({ match }) => {
             }
         }
 
-        //Change this
-        fetchType("9f72c051-203b-4712-a2c3-fccd64ec8550");
-        //fetchType(match.params.id)
+        fetchType(match.params.id);
     }, [match.params.id]);
-
-
-    const handleTypeChange = (index: number, e: any) => {
-        let data = [...typeFields];
-        data[index].type = e.target.value;
-        setTypeFields(data);
-    }
-
 
 
     return (
