dd284fad7e44c2d50f9b7ac66caab067a9a1e2cf
Alec Parkes <57089930+AlanBark@users.noreply.github.com>
2022-10-21 17:09:14 +1000

    Added qr code lookup instead of id

--- src/pages/mobile/QrScan.tsx
+++ src/pages/mobile/QrScan.tsx
@@ -1,7 +1,9 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, useIonToast } from '@ionic/react';
 import React, { useState, useEffect } from 'react';
 import { useIonRouter } from '@ionic/react';
 import { BarcodeScanner } from '@awesome-cordova-plugins/barcode-scanner';
+import { API } from 'aws-amplify';
+import { getAsset } from '../../graphql/queries';
 
 /*
   * BarcodeScanner is a plugin for Capacitor used here to scan QR Codes
@@ -10,12 +12,34 @@ import { BarcodeScanner } from '@awesome-cordova-plugins/barcode-scanner';
 const QrScan = () => {
 
   const router = useIonRouter();
+  const [toast] = useIonToast();
 
   const startScan = async () => {
     
     const result = await BarcodeScanner.scan();
     if (result.text) {
-      router.push('/asset/' + result.text);
+      try {
+        const asset: any = await API.graphql({
+          query: getAsset,
+          variables: {
+            input: {
+              QRCode: result.text
+            }
+          }
+        });
+        if (asset.data.getAsset) {
+          router.push(`/mobile/asset/${asset.data.getAsset.id}`);
+        }
+      } catch (e) {
+        const presentToast = () => {
+          toast({
+            message: 'QR Could not be read',
+            duration: 1500,
+            position: 'bottom'
+          });
+        }
+        presentToast();
+      }
     }
   };
 
