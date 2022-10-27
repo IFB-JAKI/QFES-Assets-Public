bbfbc66cd4b42a1a7d8aab6e9d101c6a8002ca06
Alec Parkes <57089930+AlanBark@users.noreply.github.com>
2022-10-27 22:33:09 +1000

    app icon + name updates

--- src/pages/mobile/QrScan.tsx
+++ src/pages/mobile/QrScan.tsx
@@ -1,10 +1,11 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, useIonToast, IonIcon } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, useIonToast, IonIcon, IonMenuButton, IonButtons } from '@ionic/react';
 import React, { useState, useEffect } from 'react';
 import { useIonRouter } from '@ionic/react';
 import { BarcodeScanner } from '@awesome-cordova-plugins/barcode-scanner';
 import { API } from 'aws-amplify';
 import { getAsset, listAssets } from '../../graphql/queries';
 import { qrCodeOutline } from 'ionicons/icons';
+import Header from '../../components/Header';
 
 /*
   * BarcodeScanner is a plugin for Capacitor used here to scan QR Codes
@@ -39,14 +40,23 @@ const QrScan = () => {
     }
   };
 
+  const menuToggle = () => {
+    document.querySelector('ion-menu')?.open();
+  }
+
   return (
     <IonPage>
+      <>
+        <IonButtons slot="start" className='lg:hidden'>
+          <IonMenuButton className="text-primary-200" autoHide={false} onClick={() => menuToggle} />
+        </IonButtons>
       <div className='flex justify-center items-center h-full'>
           <div className='text-2xl rounded-lg cursor-pointer bg-orange drop-shadow-sm flex justify-center items-center text-center flex-col' style={{width: "50vw", height: "50vw"}} onClick={startScan}>
             <IonIcon icon={qrCodeOutline} style={{fontSize: '6rem'}} />
             <p className='mt-1'>Scan QR Code</p>
           </div>
       </div>
+      </>
     </IonPage>
   );
 };
