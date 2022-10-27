780016984402f9214c149981839c26e223a642bd
Stormers64 <joshdaniell@icloud.com>
2022-10-27 21:08:47 +1000

    Home Page Makeover

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,11 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonCard, IonItem, IonThumbnail, IonCardHeader, IonCardSubtitle, IonCardTitle, IonSplitPane, IonBackButton, IonCardContent, IonIcon, IonLabel, useIonRouter } from '@ionic/react';
+import { IonContent, IonGrid, IonRow, IonCol, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonCard, IonItem, IonThumbnail, IonCardHeader, IonCardSubtitle, IonCardTitle, IonSplitPane, IonBackButton, IonCardContent, IonIcon, IonLabel, useIonRouter } from '@ionic/react';
 import Header from '../components/Header';
 import SideBar from '../components/SideBar/SideBar';
 import React, { useEffect, useState } from 'react'
 import BackButton from '../components/BackButton'
 import { listAssetLocations, listAssets, listAssetStatuses, listAssetTypes } from '../graphql/queries';
 import { API } from 'aws-amplify';
+import { documentTextOutline, gitNetworkOutline, homeOutline, listOutline, logOutOutline, statsChartOutline } from 'ionicons/icons'
 
 interface HomeProps {
   user: any;
@@ -51,7 +52,8 @@ const Home = ({ user }: HomeProps) => {
           location: (asset.assetlocaID) ? validateID(asset.assetlocaID, 'locationName', allLocations) : null,
           typeID: asset.typeID,
           type: (asset.typeID) ? validateID(asset.typeID, 'typeName', allTypes) : null,
-          updated: asset.updatedAt
+          updated: asset.updatedAt,
+          QRCode: asset.QRCode
         }
       });
       setAssets(formatted);
@@ -129,12 +131,44 @@ const Home = ({ user }: HomeProps) => {
 
       <div className="grid grid-cols-1">
         <div className="h-full bg-white p-4 m-4 rounded-lg shadow col-span-2" key={1}>
-          <IonButton routerLink='/Search' size="large">Search Assets</IonButton>
-          <IonButton routerLink='/Groups' size="large">Search Types</IonButton>
-          <IonButton routerLink='/Types' size="large">Search Groups</IonButton>
-          <IonButton routerLink='/Reports' size="large">Reports</IonButton>
+          <div className="grid grid-cols-4">
+            <div>
+              <IonButton routerLink='/Search' expand="block" size="large">
+                <IonIcon slot="start" icon={listOutline}></IonIcon>
+                Search Assets
+              </IonButton>
+            </div>
+            <div>
+              <IonButton routerLink='/Groups' expand="block" size="large">
+                <IonIcon slot="start" icon={statsChartOutline}></IonIcon>
+                Search Types
+              </IonButton>
+            </div>
+            <div>
+              <IonButton routerLink='/Types' expand="block" size="large">
+                <IonIcon slot="start" icon={gitNetworkOutline}></IonIcon>
+                Search Groups
+              </IonButton>
+            </div>
+            <div>
+              <IonButton routerLink='/Reports' expand="block" size="large">
+                <IonIcon slot="start" icon={documentTextOutline}></IonIcon>
+                Reports
+              </IonButton>
+            </div>
+          </div>
         </div>
       </div>
+      {/* <ion-card>
+  <ion-card-header>
+    <ion-card-title>Card Title</ion-card-title>
+    <ion-card-subtitle>Card Subtitle</ion-card-subtitle>
+  </ion-card-header>
+
+  <ion-card-content>
+    Here's a small text description for the card content. Nothing more, nothing less.
+  </ion-card-content>
+</ion-card> */}
       <IonContent>
         {
           assets.map((asset: any) => {
@@ -142,16 +176,37 @@ const Home = ({ user }: HomeProps) => {
               <div key={asset.id}>
                 <IonCard>
                   <IonItem detail routerLink={"/asset/" + asset.id}>
+                    {/* <div className="grid grid-cols-3 grid-rows-1 gap-4">
+                      <div className="col-span-2"> */}
+                    <IonGrid>
+                      <IonRow>
+                        <IonCol>
+                          <IonCardHeader>
+                            <IonCardTitle><h1 className="text-2xl font-montserrat font-bold text-black bg-white md:text-left">{asset.name}</h1></IonCardTitle>
 
-                    <IonThumbnail slot="start">
+                            <IonCardSubtitle>
+                              {asset.QRCode ? (<h1 className="text-2xl font-montserrat text-black bg-white md:text-left">{asset.QRCode}</h1>) : <h1 className="text-2xl font-montserrat text-red bg-white md:text-left">No QR Given</h1>}
+                            </IonCardSubtitle>
+                          </IonCardHeader>
+                        </IonCol>
+                        <IonCol>
+                          <IonCardContent>
+                            {(asset.status === "Available") ? (<h1 className="text-2xl font-montserrat font-bold text-green-600 bg-white md:text-left">{asset.status}</h1>)
+                              : (asset.status === 'On Loan') ?
+                                (<h1 className="text-2xl font-montserrat font-bold text-red bg-white md:text-left">{asset.status}</h1>)
+                                : (asset.status === "Archived") ? <h1 className="text-2xl font-montserrat font-bold text-orange bg-white md:text-left">{asset.status}</h1>
+                                  : <h1 className="text-2xl font-montserrat font-bold text-primary-200 text-blue bg-white md:text-left">{asset.status}</h1>}
+                          </IonCardContent>
+                        </IonCol>
+                      </IonRow>
+                    </IonGrid>
+                    {/* </div> */}
+                    {/* <IonThumbnail slot="start">
                       <img src="https://www.australiancomputertraders.com.au/assets/full/HP850G5i52-r.jpg?20220226055643" />
-                    </IonThumbnail>
-                    <IonCardHeader>
-                      <IonCardSubtitle color="danger">On Loan</IonCardSubtitle>
-                      <IonCardTitle>{asset.name}</IonCardTitle>
-                      {/* <IonIcon name="arrow-dropright" size="large" color="white" slot="end"></IonIcon> */}
-                    </IonCardHeader>
-
+                    </IonThumbnail> */}
+                    {/* <div className="col-auto"> */}
+                    {/* </div> */}
+                    {/* </div> */}
                   </IonItem>
                 </IonCard>
               </div>
@@ -165,3 +220,5 @@ const Home = ({ user }: HomeProps) => {
 };
 
 export default Home;
+
+<div className="grid grid-cols-1 md:grid-cols-3 gap-4"></div>
